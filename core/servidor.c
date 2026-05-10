17a270f58a8dcd5531916eef0cb3a18f1e3e179a
André Akira <andreakirapic@gmail.com>
2026-05-10 18:55:11 -0300

    feat: loop infinito leitura

--- core/servidor.c
+++ core/servidor.c
@@ -6,10 +6,44 @@
 void servidor_executa(int socket)
 {
     printf("executando em modo servidor\n");
-    
+
     struct mensagem_t msg;
-    printf("aguardando msg...\n");
-    
-    mensagem_recebe(socket, &msg);
-    mensagem_imprime(&msg);
+
+    while (1)
+    {
+        printf("aguardando msg...\n");
+
+        int status = mensagem_recebe(socket, &msg);
+
+        if (status > 0)
+        {
+            // verificação crc
+            unsigned char crc = crc8_gera(msg.dados, msg.tamanho);
+            if (crc != msg.crc)
+            {
+                printf("Erro CRC\n"); // por hora apenas ignoramos (teste)
+                continue;
+            }
+            switch (msg.tipo)
+            {
+            // movimentação:
+            case MSG_MOV_CIMA:
+            case MSG_MOV_BAIXO:
+            case MSG_MOV_DIR:
+            case MSG_MOV_ESQ:
+                mensagem_imprime(&msg);
+                break;
+
+            // fim:
+            case MSG_FIM:
+                mensagem_imprime(&msg);
+                return;
+
+            // erro:
+            default:
+                printf("erro");
+                break;
+            }
+        }
+    }
 }
