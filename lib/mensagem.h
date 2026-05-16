974978d76333d18bfa3c21b1b6fb14cc6f0e9911
André Akira <andreakirapic@gmail.com>
2026-05-16 17:28:09 -0300

    feat: timeout

--- lib/mensagem.h
+++ lib/mensagem.h
@@ -4,6 +4,9 @@
 #define MSG_MARCADOR_INICIO 0b01111110
 #define MAX_DADOS 31
 
+#define TIME_OUT_SEND 1000 // 1 segundo (teste)
+#define TIME_OUT_GET 10000 // 10 segundos (teste)
+
 enum tipo_msg_t
 {
     MSG_ACK = 0,
@@ -25,20 +28,23 @@ enum tipo_msg_t
 // estrutura que define uma mensagem
 struct mensagem_t
 {
-    unsigned char marcador_inicio;    // 01111110
-    unsigned char tamanho;            // tamanho da região de dados (0-31)
-    unsigned char sequencia;          // número da sequência (0-63)
-    unsigned char tipo;               // tipo da mensagem (definidos num ENUM)
-    unsigned char dados[MAX_DADOS];   // área de dados
-    unsigned char crc;                // método de detecção de erros
+    unsigned char marcador_inicio;  // 01111110
+    unsigned char tamanho;          // tamanho da região de dados (0-31)
+    unsigned char sequencia;        // número da sequência (0-63)
+    unsigned char tipo;             // tipo da mensagem (definidos num ENUM)
+    unsigned char dados[MAX_DADOS]; // área de dados
+    unsigned char crc;              // método de detecção de erros
 };
+
+// função auxiliar
+long long timestamp();
 
 struct mensagem_t *mensagem_cria(unsigned char tamanho,
                                  enum tipo_msg_t tipo, unsigned char *dados);
 
 int mensagem_envia(int socket, struct mensagem_t *msg);
 
-int mensagem_recebe(int socket, struct mensagem_t *msg);
+int mensagem_recebe(int socket, struct mensagem_t *msg, int timeoutMillis);
 
 unsigned char crc8_gera(unsigned char *dados, unsigned char tamanho);
 
