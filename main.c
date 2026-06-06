df4fda51f6a125b1b83ff9ab177f1c7165225ace
André Akira <andreakirapic@gmail.com>
2026-06-06 17:58:29 -0300

    feat: implementação de mapa pelo servidor

--- main.c
+++ main.c
@@ -9,13 +9,13 @@ int main(int argc, char *argv[])
 {
     if (argc < 2)
         return 1;
-    
+
     if (argv[1][0] != '-')
         return 1;
-    
+
     char modo = argv[1][1]; // servidor ou cliente
     int modo_opcao;
-    
+
     if (modo == 's')
         modo_opcao = MODO_SERVIDOR;
     else if (modo == 'c')
@@ -25,21 +25,24 @@ int main(int argc, char *argv[])
         printf("opção inválida.\n");
         return 1;
     }
-    
+
     char nome_interfaces[MAX_INTERFACES][MAX_INTERFACE_NOME];
     int qtd, i, socket;
-    
+
     qtd = interface_imprime(nome_interfaces);
     i = interface_escolhe(qtd);
-    
+
     printf("criando socket para a interface %s...\n", nome_interfaces[i]);
-    
+
     socket = raw_socket_cria(nome_interfaces[i]);
-    
+
     if (modo_opcao == MODO_SERVIDOR)
-        servidor_executa(socket);
+    {
+        char *caminho_mapa = (argc >= 3) ? argv[2] : NULL;
+        servidor_executa(socket, caminho_mapa);
+    }
     else
         cliente_executa(socket);
-    
+
     return 0;
 }
